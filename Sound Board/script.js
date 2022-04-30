fda2cea125787720782590ce247acf1278a7d27d
Pranav Gautam <prnvgautam15@gmail.com>
2022-05-01 00:03:32 +0530

    Day 7

---
+++ Sound Board/script.js
@@ -0,0 +1,20 @@
+const sounds = ['applause', 'boo', 'gasp']
+
+sounds.forEach(sound =>{
+    const button = document.createElement('button');
+    button.classList.add('btn')
+    button.innerText = sound;
+    button.addEventListener('click',()=>{
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(button);
+})
+
+function stopSongs(){
+    sounds.forEach(sound =>{
+        const song =document.getElementById(sound);
+        song.pause()
+        song.currentTime = 0;
+    })
+}
